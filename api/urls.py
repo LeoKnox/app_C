6c1b4a24448581448baa7f093e06979209885fa7
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-13 15:55:27 -0700

    Add files via upload

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.ApiOverview, name='home'),
-    path('create/', views.add_rooms, name='add-items'),
-    path('all/', views.view_rooms, name='view_items'),
+    path('create/', views.add_rooms, name='add-rooms'),
+    path('all/', views.view_rooms, name='view_rooms'),
+    path('update/<int:pk>', views.update_rooms, name='updates-rooms'),
     path('room/<int:pk>/delete/', views.delete_rooms, name='delete-rooms'),
 ]
