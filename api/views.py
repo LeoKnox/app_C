9aa0cb5be6f86b5941c05795df2c2cbc560fb1f4
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-12 21:58:42 -0700

    Add files via upload

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def ApiOverview(request):
+    api_urls = {
+        'all_items': '/',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/item/pk/delete'
+    }
+
+return Response(api_urls)
