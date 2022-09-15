dd266b75c3e711978bfd9128fd614b2bbee85f6f
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-14 19:58:06 -0700

    Add files via upload

--- frontend/src/rooms.js
+++ frontend/src/rooms.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 function Rooms() {
 
-    const [rooms, setRooms] = "";
+    const [rooms, setRooms] = useState("");
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/all/')
             .then(res => {
                 const data = res.data;
                 setRooms(data);
-                console.log(rooms + "222");
+                console.log(rooms);
             })
             .catch(err => {})
     }, [])
