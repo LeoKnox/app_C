8abff7aae9d13ff32e0315d34dc9297637f2d422
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-14 18:44:41 -0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Rooms from './rooms.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Create Rooms Here</h1>
+        <Rooms />
       </header>
     </div>
   );
