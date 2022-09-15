40ef765a5ecd7b61b5ab154ca2da1f8141771f55
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-14 22:06:44 -0700

    Add files via upload

--- frontend/src/create.js
+++ frontend/src/create.js
@@ -2,10 +2,15 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Create() {
+    function create_room(e) {
+        e.preventDefault();
+        console.log("SBUMITTED");
+    }
+
     return (
         <>
         <p>Create Room</p>
-        <input type="submit" value="Submit" />
+        <input onSubmit={create_room} type="submit" value="Submit" />
         </>
     )
 }
