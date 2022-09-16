c773fb6988ac485758bb7635a6a3065a3aeca451
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-15 18:11:33 -0700

    Add files via upload

--- frontend/src/create.js
+++ frontend/src/create.js
@@ -24,8 +24,8 @@ function Create() {
         let newRoom = {name, description, length, width};
         axios({
             method: 'post',
-            url: 'http://127.0.0.1:8000/api/create',
-            data: newRoom[0]
+            url: 'http://127.0.0.1:8000/api/create/',
+            data: newRoom
         });
         e.preventDefault();
         console.log(newRoom);
