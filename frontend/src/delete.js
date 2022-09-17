13d626f35c4cdca4033b7f88887ebbd3136111c4
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-16 20:59:22 -0700

    Add files via upload

--- frontend/src/delete.js
+++ frontend/src/delete.js
@@ -4,7 +4,7 @@ function Delete(id) {
     function delete_room(e) {
         axios({
             method:'delete',
-            url: 'http://127.0.0.1:8000/api/room/'+{id}+'/delete/'
+            url: 'http://127.0.0.1:8000/api/room/'+id+'/delete/'
         });
     }
     return(
@@ -12,6 +12,7 @@ function Delete(id) {
         <form onSubmit={delete_room}>
             <input type="submit" value="Delete" />
         </form>
+        <button onClick={delete_room}>Delete</button>
         </>
     )
 }
