52164ebaf478d40b6e45c3125cb7a474f8483279
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-18 19:46:50 -0700

    Add files via upload

--- frontend/src/single.js
+++ frontend/src/single.js
@@ -4,6 +4,12 @@ import { useParams } from "react-router-dom";
 
 function Single() {
     const [room, setRoom] = useState("");
+    const [name, setName] = useState("");
+
+    const nameChange = (e) => {
+        setName(e.target.value);
+    }
+    
     let params = useParams();
     useEffect(() => {
     axios.get(`http://127.0.0.1:8000/api/room/${params.roomid}/`)
@@ -15,9 +21,19 @@ function Single() {
     }, [])
     console.log(room);
 
+    function update_room(e) {
+        console.log("red");
+    }
+
     return (
         <>
             <h3>A single room</h3>
+            <form onSubmit={update_room}>
+                <label>Name: </label>
+                <input type="text" value={name}
+                    placeholder={room.name}
+                    onchange={(e) => {nameChange(e)}} /><br />
+            </form>
             <p>Name: {room.name}</p>
             <p>Description: {room.description}</p>
             <p>Width: {room.width} x Length: {room.length}</p>
