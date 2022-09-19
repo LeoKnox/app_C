3bb4760c15edb4f7ade9a8924ebb16f36e218407
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-18 20:05:39 -0700

    Add files via upload

--- frontend/src/single.js
+++ frontend/src/single.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useParams } from "react-router-dom";
 
 function Single() {
-    const [room, setRoom] = useState("");
+    const [room, setRoom] = useState(false);
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [length, setLength] = useState("");
