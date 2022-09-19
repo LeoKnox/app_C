60cf2b46fbe9cdcf8a03ab17740eed4328baae1d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-18 20:31:56 -0700

    Add files via upload

--- frontend/src/single.js
+++ frontend/src/single.js
@@ -18,10 +18,11 @@ function Single() {
     axios.get(`http://127.0.0.1:8000/api/room/${params.roomid}/`)
         .then(res => {
             const data = res.data;
+            setName(data.name);
             setRoom(data);
         })
         .catch(err => {})
-    }, [room.name])
+    }, [])
     console.log(room);
 
     function update_room(e) {
@@ -35,7 +36,6 @@ function Single() {
             <form onSubmit={update_room}>
                 <label>Name: </label>
                 <input type="text" value={name}
-                    placeholder={room.name}
                     onchange={(e) => {nameChange(e)}} /><br />
             </form>
             <p>Name: {room.name}</p>
