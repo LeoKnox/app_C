27cde75a7b3b2d2029adbb0ca9bbd33683fc4fa0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-20 22:20:30 -0700

    Add files via upload

--- frontend/src/nav.js
+++ frontend/src/nav.js
@@ -2,10 +2,9 @@ import {useNavigate} from 'react-router-dom';
 
 function NavBar() {
     const navigate = useNavigate();
-    function goHome() {navigate('/')};
 
     return (
-        <button onClick={goHome}>Home</button>
+        <button onClick={(() => {navigate("/")})}>Home</button>
     )
 }
 
