11ec0e7704478ef3dcd3bd35d7b641309b0ab8c4
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-20 22:25:53 -0700

    Add files via upload

--- frontend/src/nav.js
+++ frontend/src/nav.js
@@ -1,11 +1,9 @@
-import { Link, Router } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
 
     return (
-        <Router>
-            <Link to="/">Home</Link>
-        </Router>
+        <NavLink to="/">Home</NavLink>
     )
 }
 
