da6f36d6f6b2a849a071de5b588a9a308487eb90
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-22 22:23:43 -0700

    Add files via upload

--- frontend/src/delete.js
+++ frontend/src/delete.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 function Delete(id) {
+    let history = useHistory();
+
     function delete_room(e) {
         axios({
             method:'delete',
             url: 'http://127.0.0.1:8000/api/room/'+id.id+'/delete/'
         });
+        history.push("/home");
     }
     return(
         <>
