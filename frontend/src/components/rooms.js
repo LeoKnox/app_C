98c81c8ad8ed89fb6e50c2516b5b84d960b0edb8
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-26 21:22:32 -0700

    Add files via upload

--- frontend/src/components/rooms.js
+++ frontend/src/components/rooms.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import GetData from './getData.js';
 
 function Rooms() {
+    const [rooms, setRooms] = useState([]);
     console.log(GetData.getRooms());
+    GetData.getRooms().then(
+        res => {
+            const data = res.data;
+            setRooms(data);
+            console.log(rooms);
+        }, [])
+        .catch(err => {})
     return (
         <>
             <h1>some rooms coming</h1>
